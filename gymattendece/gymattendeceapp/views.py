b3d843760987a5be9c8a7b99df05a52e81e4d30f
USAMAWIZARD <usamwizard@gmail.com>
2020-03-10 21:23:44 +0530

    bug fixes

--- gymattendece/gymattendeceapp/views.py
+++ gymattendece/gymattendeceapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from Models import gym
 # Create your views here.
 def markattendence(request):
 	return render(request,"markattendence.html")
 def register(request):
 	return render(request,"registeruser.html")
+def registeruserdata(request):
+	pass
